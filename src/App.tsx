04f705011e75eaf9aee6ec222f238a0acf82ed36
Leonardo <lcat140507@gmail.com>
2026-05-18 20:17:17 +0100

    integrar authprovider no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeProvider";
+import { AuthProvider } from "./context/AuthProvider"; // Adicionado
 import Home from "./pages/Home";
 import Feed from "./pages/Feed";
 import Profile from "./pages/Profile";
@@ -8,14 +9,16 @@ import Backoffice from "./pages/Backoffice";
 export default function App() {
   return (
     <ThemeProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/backoffice" element={<Backoffice />} />
-        </Routes>
-      </Router>
+      <AuthProvider> {/* Adicionado */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/backoffice" element={<Backoffice />} />
+          </Routes>
+        </Router>
+      </AuthProvider> {/* Adicionado */}
     </ThemeProvider>
   );
 }
